b2bd8e7a19e5c624bacaefe0571188f83954071e
futurist <jamesyang999@gmail.com>
2017-07-26 08:02:52 +0800

    1.0.4 package.json: browser field using umd.js

--- dist/cssobj-plugin-keyframes.iife.js
+++ dist/cssobj-plugin-keyframes.iife.js
@@ -6,9 +6,7 @@ var cssobj_plugin_keyframes = (function () {
 // check n is numeric, or string of numeric
 
 
-function own(o, k) {
-  return {}.hasOwnProperty.call(o, k)
-}
+
 
 // set default option (not deeply)
 
@@ -20,9 +18,6 @@ function own(o, k) {
 
 
 // repeat str for num times
-
-
-// don't use String.prototype.trim in cssobj, using below instead
 
 
 // random string, should used across all cssobj plugins
@@ -38,11 +33,7 @@ var random = (function () {
 
 
 // ensure obj[k] as array, then push v into it
-function arrayKV (obj, k, v, reverse, unique) {
-  obj[k] = k in obj ? [].concat(obj[k]) : [];
-  if(unique && obj[k].indexOf(v)>-1) { return }
-  reverse ? obj[k].unshift(v) : obj[k].push(v);
-}
+
 
 // replace find in str, with rep function result
 
@@ -50,7 +41,13 @@ function arrayKV (obj, k, v, reverse, unique) {
 // get parents array from node (when it's passed the test)
 
 
-// split selector etc. aware of css attributes
+// split selector with comma, aware of css attributes
+
+
+// split selector with splitter, aware of css attributes
+
+
+// split char aware of syntax
 
 
 // checking for valid css value
